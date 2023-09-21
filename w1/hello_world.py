4e99fe50dcd6947886453d5ce63920e405eed5e1
Jarron Cho <jarroncho@ptcom.com.tw>
2023-09-21 16:43:01 +0800

    w1 init

---
+++ w1/hello_world.py
@@ -0,0 +1,23 @@
+
+import sys;
+
+# hello world 
+
+print('hello world line 1');
+
+print('hello world line 2');
+
+print('hello world line 3');
+
+# print hellowrld with variable 
+def hello():
+    print('hello world line 4');
+
+
+def showpythonversion():
+    print('python version is :', sys.version);
+
+# call hello function
+hello();
+
+showpythonversion();
