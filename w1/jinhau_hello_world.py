7c1e8145834757c7e83dba4e42377568801f3a8b
Jinhau <icecream103018@gmail.com>
2023-09-22 11:49:51 +0800

    test
    1090605梁晉豪

---
+++ w1/jinhau_hello_world.py
@@ -0,0 +1,11 @@
+
+
+# print hellowrld with variable
+def data_function(name,id):
+    # print(f'name:',name,f'id:',id)
+    print(f"name:{name},id: {id}")
+
+
+# call hello function
+data_function("梁晉豪",1090605)
+
