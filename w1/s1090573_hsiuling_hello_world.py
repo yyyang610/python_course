660ebbc6cdd642cf919724aeec9e9ad423fef4b5
Chunghsiuling <116236364+Chunghsiuling@users.noreply.github.com>
2023-09-22 14:54:44 +0800

    Create s1090573_hsiuling_hello_world.py
    test

---
+++ w1/s1090573_hsiuling_hello_world.py
@@ -0,0 +1,17 @@
+_import sys;
+
+# hello world 
+62626
+
+# print hellowrld with variable 
+def hello():
+    print('1090573 鍾秀玲')
+
+
+def showpythonversion():
+    print('python version is :', sys.version)
+
+# call hello function    
+
+hello()
+showpythonversion()
