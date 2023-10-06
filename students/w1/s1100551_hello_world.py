55e39027e6eb5e56077f988c146b0c819b96d682
yehchiahui <145738224+yehchiahui@users.noreply.github.com>
2023-10-06 11:17:27 +0800

    Add files via upload
    commit

---
+++ students/w1/s1100551_hello_world.py
@@ -0,0 +1,18 @@
+
+import sys;
+
+# hello world 
+62626
+
+# print hellowrld with variable 
+def hello():
+    print('1100551 葉家慧')
+
+
+def showpythonversion():
+    print('python version is :', sys.version)
+
+# call hello function
+hello()
+
+showpythonversion()
