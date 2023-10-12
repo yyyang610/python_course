3cc3af6bd77dd97025e1159ba75aa59ee012b17b
ddoorriiss969696 <doris_m1b@yahoo.com.tw>
2023-10-12 16:32:43 +0800

    Update 1100564_w2_grade_list.py

--- students/w2/1100564_w2_grade_list.py
+++ students/w2/1100564_w2_grade_list.py
@@ -30,15 +30,14 @@ for student in grade_list:
     student[5] = sum(student[1:5]) #計算總分並將總分存到5的列表中，也就是第6列(總分)的位置
     student[6] = student[5]/4 #計算平均，將原本存到表格(總分)位置的值拿出來除4，再存到表格(平均)的位置
 
-sorted_grade_list = sorted(grade_list, key=lambda x: x[5], reverse=True) #將學生總分(x[5])進行排序，把成績較高的學生排在前面
+sorted_grade_list = sorted(grade_list, key=lambda x: x[5],reverse=True) #將學生總分(x[5])進行排序，把成績較高的學生排在前面
 
 # 設定名次
 for i, student in enumerate(sorted_grade_list):
     student[7] = i + 1 #將排好的名次存到7，也就是表格第8列(名次)的位置，因為通常名次是1開始，故i+1才會是1開始
 
-
-# 列印學生成績和名次
-for student in sorted_grade_list:
-    print('\t'.join(map(str, student))) 
+# 印出表格
+for student in grade_list:
+    print('\t'.join(map(str, student)))
 
 
