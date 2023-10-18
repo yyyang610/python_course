f2028e1cd2d2c9852085a86e7c2a12ff2e2b12ca
Jarron Cho <jarroncho@ptcom.com.tw>
2023-10-18 23:25:19 +0800

    w4 init
    add example

--- teacher/w4/w4_example.py
+++ teacher/w4/w4_example.py
@@ -3,5 +3,11 @@ import gspread
 gc = gspread.service_account(filename='python_course_access_cred.json')
 sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1nqgiOHVyuIM1p4cBKUsi1HfmkaIhjIQdQYamGbkzOhE/edit#gid=0')
 
-print(sh.sheet1.get('A1:b15'))
+cell_range_pattern='c5:j12'
+cell_range=sh.sheet1.get(cell_range_pattern)
 
+print(cell_range)
+print(type(cell_range))    
+
+grade_list_data=list(cell_range)
+print(type(grade_list_data))
