8fe4e7539f75fe47bf3c78d5d2fc2aa3c3973bf4
s1090659 <maxbroth001@gmail.com>
2023-10-19 22:38:02 +0800

    1090659

--- students/w3/1090659_w3_grade_list.py
+++ students/w3/1090659_w3_grade_list.py
@@ -4,7 +4,7 @@ import openpyxl
 
 print("POP")
 excel_file_path = "C:\Users\max\Desktop\python\python_course\teacher\w3\grade_list.xlsx"
-workbook=openpyxl.load_wordbook(excel_file_path)
+workbook=openpyxl.load_workbook(excel_file_path)
 
 sheet_names = workbook.sheetnames
 sheet_name = workbook[sheet_names[0]]
@@ -43,21 +43,22 @@ class Account:
         excel_file_path = "C:\Users\max\Desktop\python\python_course\teacher\w3\grade_list.xlsx"
         workbook=openpyxl.load_wordbook(excel_file_path)
 
-        sheet_names = workbook.sheetnames
-        sheet_name = workbook[sheet_names[0]]
+        self.sheet_names = workbook.sheetnames
+        self.sheet_name = workbook[sheet_names[0]]
         print("Sheet names:", sheet_names)
-        sheet=workbook[sheet_name]
+        self.sheet=workbook[sheet_name]
 
-        cell_range_pattern = 'c5:j12'
-        grade_list = [[data.value for data in row] for row in cell_range_pattern]
-        cell_range = sheet[cell_range_pattern]
+        self.cell_range_pattern = 'c5:j12'
+        self.grade_list = [[data.value for data in row] for row in cell_range_pattern]
+        self.cell_range = sheet[cell_range_pattern]
         print(cell_range)
         workbook.close()
 
-    def __caculate__(self):
-        for i in range(len(grade_list)):
-        grade_list[i][5]=grade_list[i][1]+grade_list[i][2]+grade_list[i][3]+grade_list[i][4]
-        grade_list[i][6]=grade_list[i][5]/4
+    def caculate(self):
+        for i in range(len(self.grade_list)):
+            self.grade_list[i][5] = self.grade_list[i][1] + self.grade_list[i][2] + self.grade_list[i][3] + self.grade_list[i][4]
+            self.grade_list[i][6] = self.grade_list[i][5] / 4
+
     
         sort_data = sorted(grade_list, key=lambda x: x[6])
 
